e213d1d821b715d43964ce34ca644467ce1b0b31
JuanCamiloGanan <juan.ganan50831@ucaldas.edu.co>
2024-03-20 15:41:06 -0500

    Correcion bug primer ejercicio POO 2024-1

---
+++ Programacion Orientada a Objetos/gradebook/gradebookTest.java
@@ -0,0 +1,11 @@
+package edu.ucaldas.poo.gradebook;
+
+public class gradebookTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		gradeBook courseName1 = new GradeBook("numero2");
+		System.out.println(courseName1.displayMessage() + courseName1.getCourseName());
+	}
+
+}
