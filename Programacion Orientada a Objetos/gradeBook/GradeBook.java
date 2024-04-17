e733535c3f5be6b8bb3d5e9a0475d9bd46e15d50
JuanCamiloGanan <juan.ganan50831@ucaldas.edu.co>
2024-04-16 19:16:52 -0500

    Ejercicio Calculo Interes

---
+++ Programacion Orientada a Objetos/gradeBook/GradeBook.java
@@ -0,0 +1,23 @@
+package edu.ucaldas.poo.gradeBook;
+import java.util.Scanner;
+public class GradeBook {
+    private String courseName;
+
+    public GradeBook(String courseName){
+        this.courseName = courseName;
+    }
+    public String getCourseName(){
+        return courseName;
+    }
+    public void setCourseName(String courseName){
+        this.courseName = courseName;
+    }
+
+    public String displayMessage(){
+        return "Bienvenido a la lista de planillas : ";
+    }
+
+    public void determineClassAverage(){
+
+    }
+}
